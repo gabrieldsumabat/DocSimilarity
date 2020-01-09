8db5eb524fb8e2aaba11d3c093b46963a07a9723
Gabe <gabrieldsumabat@gmail.com>
2020-01-08 19:08:23 -0500

    Updated the config.py file to contain labels on configuration values.

--- code/Config.py
+++ code/Config.py
@@ -1,9 +1,12 @@
+# File Path Configuration Values
 DEFAULT_PATH = "./tmp"
 DEFAULT_OUTPUT = "."
 DEFAULT_DOC_NAME = "/documents.txt"
 
+# Luigi Configuration Values
 NAMESPACE = 'rubrikloud'
 
+# Document Configuration Values
 DELIMITER = ","
 DOC_DELIMITER = "%"
 ALLOWED_CHARACTERS = '[^a-zA-Z0-9% \\n]+'
