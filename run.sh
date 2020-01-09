38157049458cb67fe0ef485f8bb65c93bd167ab5
Gabe <gabrieldsumabat@gmail.com>
2020-01-08 19:16:27 -0500

    Added default Python Run path to be PWD in script

--- run.sh
+++ run.sh
@@ -6,6 +6,8 @@ if [ $# != 1 ]; then
   echo "Warning: No input path detected. Defaulting to ./tmp directory to search for input file."
 fi
 
+export PYTHONPATH='.'
+
 echo "Running PIP Install for Luigi"
 pip install -r ./code/requirements.txt
 echo "Running Luigi Pipeline for file: $1"
